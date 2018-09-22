e5c1965a3e16101d713a829ad56664ff35960bd9
rizalarifnugraha <43487664+rizalarifnugraha@users.noreply.github.com>
2018-09-22 19:47:58 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan7/integer/PBO210117048Latihan7Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * Nama  : Rizal Arif Nugraha
+ * Kelas : PBO2 
+ * NIM   : 10117048
+ * Deskripsi Program : Program ini berisi program untuk menampilkan data
+ * jenis-jenis tipe data bilangan bulat. 
+
+ */
+public class PBO210117048Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte  : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int   : " + i);
+        System.out.println("long  : " + l);
+        
+    }
+    
+}
